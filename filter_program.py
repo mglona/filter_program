6f1c291c640087b2e1286d45fe9a46941f1227c9
Mage Ona <marygracelona@gmail.com>
2018-01-29 20:03:53 +0800

    filter program

---
+++ filter_program.py
@@ -0,0 +1,12 @@
+fruits = ['apple', 'orange', 'mango']
+
+def is_fruit(thing, list):
+	if thing in list:
+		return True
+	else:
+		return False
+		
+
+print filter(None, fruits)
+filter(None, fruits)
+
